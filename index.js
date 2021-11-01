21b63368f30b33d46154c5d11c651ffbfe7cd7f7
habibshrabon <snhabib777@gmail.com>
2021-11-01 14:04:24 +0600

    server is working

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ewzy7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("online_shop");
+    const productCollection = database.collection("products");
+    // console.log("database connected successfully");
+    const orderCollection = database.collection("orders");
+
+    //GET PRODUCTS API
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const page = req.query.page;
+      const size = parseInt(req.query.size);
+      let products;
+      const count = await cursor.count();
+
+      if (page) {
+        products = await cursor
+          .skip(page * size)
+          .limit(size)
+          .toArray();
+      } else {
+        products = await cursor.toArray();
+      }
+
+      res.send({
+        count,
+        products,
+      });
+    });
+
+    //USE POST TO GET DATA BY KEYS
+    app.post("/products/byKeys", async (req, res) => {
+      const keys = req.body;
+      const query = { key: { $in: keys } };
+      const products = await productCollection.find(query).toArray();
+      res.json(products);
+    });
+
+    //Add orders API
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      // console.log(order);
+      const result = await orderCollection.insertOne(order);
+      res.json(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Ema jon server is running");
+});
+
+app.listen(port, () => {
+  console.log("Server Running at port", port);
+});
